c7b6a07237c7b1bdfaf9c107a019f3ece8f606d2
yan.liang <yliang012913304@gmail.com>
2021-04-21 21:08:07 +0200

    feat(button): step4 - add new component input button

---
+++ src/app/input-button-unit/input-button-unit.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-input-button-unit',
+  template: `
+    <p>
+      input-button-unit works!
+    </p>
+  `,
+  styleUrls: ['./input-button-unit.component.scss']
+})
+export class InputButtonUnitComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
